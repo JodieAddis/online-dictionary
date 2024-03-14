87cc59998eea2513ee312cec3e8cafc96d44824b
JodieAddis <jodie.addis.92@gmail.com>
2024-03-14 14:28:39 +0100

    Form correction: from controlled to uncontrolled

--- src/container/SearchContainer/index.tsx
+++ src/container/SearchContainer/index.tsx
@@ -4,25 +4,36 @@ import DictionaryPhonetic from "../../components/Dictionary/DictionaryPhonetic";
 import DictionarySource from "../../components/Dictionary/DictionarySource";
 import DictionarySynonym from "../../components/Dictionary/DictionarySynonym";
 
-import { FormEvent, useState } from "react";
+import { FormEvent, useRef, useState } from "react";
 import IconSearch from "../../icons/IconSearch";
 
 const Container = () => {
   const [currentWord, setCurrentWord] = useState<string>("");
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setCurrentWord(event.target.value);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (inputRef.current !== null) {
+      console.log(inputRef.current.value);
+      setCurrentWord(inputRef.current.value);
+    }
   };
+
   return (
     <>
-      <form action="" className="my-10 flex w-full self-center lg:w-3/6">
+      <form
+        action=""
+        className="my-10 flex w-full self-center lg:w-3/6"
+        onSubmit={handleSubmit}
+      >
         <input
           type="text"
           placeholder="Search a word..."
           id="input-text"
           className="w-full rounded-l-lg bg-Gallery py-4 font-bold placeholder-SilverChalice placeholder:pl-5"
-          onChange={handleChange}
-          value={currentWord}
+          defaultValue={currentWord}
+          ref={inputRef}
         />
         <button className="rounded-r-lg bg-Gallery pr-5" type="submit">
           <IconSearch />
@@ -42,7 +53,7 @@ const Container = () => {
         </>
       </div>
       <div className="flex justify-center">
-        <DictionarySynonym wordInput={currentWord} />
+        <DictionarySynonym wordInput={"test"} />
       </div>
       <div className="my-8 flex h-[1px] self-center rounded-full bg-SilverChalice lg:w-3/6"></div>
       <DictionarySource />
@@ -51,24 +62,3 @@ const Container = () => {
 };
 
 export default Container;
-
-/*
-      <form
-        action=""
-        className="my-10 flex w-full self-center lg:w-3/6"
-        onSubmit={handleClick}
-      >
-        <input
-          type="text"
-          placeholder="Search a word..."
-          id="input-text"
-          className="w-full rounded-l-lg bg-Gallery py-4 font-bold placeholder-SilverChalice placeholder:pl-5"
-          onChange={(e) => {
-            e.target.value;
-          }}
-        />
-        <button className="rounded-r-lg bg-Gallery pr-5" type="submit">
-          <IconSearch />
-        </button>
-      </form>
-*/
